22b589b58074c184b4c0b7135fb7513ca55e14b3
friaf <shovel.irena@gmail.com>
2023-02-13 23:01:40 +0000

    newscore function

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game } = require("../game");
+const { game, newGame, showScore } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -28,4 +28,27 @@ describe("game object contains correct keys", () => {
     test("choices contains correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
+});
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = ["button1", "button2"];
+        game.currentGame = ["button1", "button2"];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("shoul clear computer sequence array", () => {
+        expect(game.currentGame.length).toBe(0);
+    });
+    test("should set game score to zero", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    test("should display 0 for element with id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
+    });
+
 })
