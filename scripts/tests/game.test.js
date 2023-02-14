245b10ce2342f89ae3c83ab7ac57d7db9fb952ba
friaf <shovel.irena@gmail.com>
2023-02-14 14:13:38 +0000

    add turn functuin

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame, showScore } = require("../game");
+const { game, newGame, showScore, addTurn } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -41,8 +41,8 @@ describe("newGame works correctly", () => {
     test("should set game score to zero", () => {
         expect(game.score).toEqual(0);
     });
-    test("shoul clear computer sequence array", () => {
-        expect(game.currentGame.length).toBe(0);
+    test("shoul be one elemnt in computers array", () => {
+        expect(game.currentGame.length).toBe(1);
     });
     test("should set game score to zero", () => {
         expect(game.playerMoves.length).toBe(0);
